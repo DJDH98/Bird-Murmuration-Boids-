7eef2e09277c152f0e06605deca9c2f17c27e5a0
DJDH98 <dalenharris5@hotmail.com>
2026-05-25 15:48:54 +0100

    build: set base path for deployment
    Configure the base URL to ensure correct asset loading when hosted on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Bird-Murmuration-Boids-/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
